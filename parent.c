f48400389f7980313cb274d5a1d6deb7db702936
Thomas Brunner <thomas.brunner@epitech.eu>
2012-04-24 16:30:33 +0200

    nardin

--- parent.c
+++ parent.c
@@ -1,10 +1,19 @@
 
+#include <sys/ptrace.h>
 #include <sys/wait.h>
+#include <stdio.h>
 #include "strace.h"
+
+static void	trace_process(int pid)
+{
+
+}
 
 void		exec_parent(int pid)
 {
   int		status;
 
-  wait4(pid, &status, 0, NULL);
+  ptrace(PTRACE_ATTACH, pid, NULL, 0);
+  wait4(pid, &status, WUNTRACED, NULL);
+  trace_process(pid);
 }
